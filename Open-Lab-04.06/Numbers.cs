ea6e844f2b456215b58cd4e5876b384d2161fcf2
Viktor <krpectv57@gmail.com>
2020-10-24 12:06:53 +0200

    _

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Open_Lab_04._06
 {
@@ -6,7 +9,19 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            
+            var a = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    a.Add (numbers[i]);                    
+                }
+
+            }
+
+            return a.ToArray();
+            
         }
     }
 }
